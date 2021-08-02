20f0462573fe19ac4ae109cc50276325681bd2ff
mayconrcampos <maycon.campos@gmail.com>
2021-08-02 16:10:19 -0300

    deu

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+
+$conexao = "localhost";
+$user = "root";
+$senha = "5DaJ10.,Xw,8";
+$DBnome = "usuarios";
+$port = 3306;
+
+$conn = new PDO()
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD - PDO</title>
+</head>
+<body>
+    <h1>CRUD com PDO - Cadastrar Usuário e Email</h1>
+
+    <hr>
+
+    <form action="" method="post">
+        <label for="">Nome</label>
+        <input type="text" name="nome">
+        <br><br>
+
+        <label for="">Email</label>
+        <input type="email" name="email">
+        <br><br>
+
+        <input type="submit" value="Cadastrar">
+    </form>
+    
+</body>
+</html>
